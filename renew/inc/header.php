f166d719002ea9783bb1df8c577fea36b2823c76
Tomotada Saiugusa <nirot1r@g-second.net>
2018-04-10 23:12:25 +0900

    #2 フロント部分の作成とスコアリストの実装

---
+++ renew/inc/header.php
@@ -0,0 +1,51 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: nirot1r
+     * Date: 2018/04/10
+     * Time: 21:23
+     */
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+            integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+            crossorigin="anonymous">
+        <link
+            rel="stylesheet"
+            href="css/uths.css">
+        <style>
+        </style>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="#">uths.xyz/ukwo</a>
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="ナビゲーションの切替">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <!--<li class="nav-item active">
+                        <a class="nav-link" href="#">ホーム <span class="sr-only">(現位置)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">リンク1</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">リンク2</a>
+                    </li>-->
+                </ul>
+                <span class="navbar-text">
+                    ログイン中 : 三枝 義忠
+                </span>
+            </div>
+        </nav>
+
+<?php include "inc/footer.php"; ?>
---
+++ renew/index.php
@@ -0,0 +1,81 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * User: nirot1r
+     * Date: 2018/04/10
+     * Time: 21:26
+     */
+
+    include "inc/header.php";
+    require_once "../api/API.php";
+        $API = new nirot1r\API();
+
+    if(isset($_GET["p"]) && $_GET["p"] !== "") {
+        $p = intval($_GET["p"]);
+        if($p <= 0) {
+            $p = 1;
+        }
+    } else {
+        $p = 1;
+    }
+
+    $scoresList = $API::getScoreList();
+    $scoresID = array_slice($scoresList, ($p -1) * 50, 50);
+    $scoresCount = count($scoresList);
+    $allPages = intval(ceil($scoresCount / 50));
+
+?>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">スコア管理システム</h1>
+            </div>
+        </div>
+
+        <div class="container">
+            <h2>登録されているスコア <small class="badge badge-secondary"><?= $scoresCount ?>件</small></h2>
+
+            <ul class="pagination justify-content-center">
+                <li class="page-item"><a class="page-link" href="?p=<?= ($p - 1) > 0 ? $p-1 : 1; ?>">&laquo;</a></li>
+                <li class="page-item"><a class="page-link" href="?p=<?= $p < $allPages ? $p + 1 : $allPages;?>">&raquo;</a></li>
+            </ul>
+            <p class="text-center"><?= $scoresCount ?>件中 <?= ($p -1) * 50 + 1 ?> 〜 <?= $p * 50 ?> 件</p>
+
+            <table class="table table-responsive">
+                <thead class="table table-sm">
+                    <tr>
+                        <th scope="row">#</th>
+                        <th scope="row">曲名</th>
+                        <th scope="row">格納場所</th>
+                        <th scope="row">検索用</th>
+                        <th scope="row">状態</th>
+                        <th scope="row">所有団体</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php
+                    foreach($scoresID as $item):
+                        $scoreData = $API::loadScoreInfo($item);
+                ?>
+                    <tr class="success">
+                        <th scope="row"><?= $item ?></th>
+                        <td><a href="./scores.php?scoreID=<?= $item ?>"><?= $scoreData["scoreName"] ?></a></td>
+                        <td><?= $scoreData["scoreInitial"] ?></td>
+                        <td><?= $scoreData["scoreKana"] ?></td>
+                        <td>部室に格納済み</td>
+                        <td>栃木県立宇都宮工業高等学校 音楽部</td>
+                    </tr>
+
+                <?php
+                    endforeach;
+                ?>
+                </tbody>
+            </table>
+
+            <p class="text-center"><?= $scoresCount ?>件中 <?= ($p -1) * 50 + 1 ?> 〜 <?= $p * 50 ?> 件</p>
+            <ul class="pagination justify-content-center">
+                <li class="page-item"><a class="page-link" href="?p=<?= ($p - 1) > 0 ? $p-1 : 1; ?>">&laquo;</a></li>
+                <li class="page-item"><a class="page-link" href="?p=<?= $p < $allPages ? $p + 1 : $allPages;?>">&raquo;</a></li>
+            </ul>
+
+        </div>
